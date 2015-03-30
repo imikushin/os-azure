8fc6ab7ded206ce9414f8314dbd3b839fb6d7c78
Ivan Mikushin <i.mikushin@gmail.com>
2015-03-30 19:15:29 +0500

    init

---
+++ drive-rancheros-install.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+## make sure /dev/sdc exists
+# ls -lAh /dev/disk/by-id/
+
+docker run --privileged --net=host -it --entrypoint=/scripts/set-disk-partitions imikushin/os-installer /dev/sdc
+# vi agent-init.sh
+# chmod +x agent-init.sh
+docker run --privileged --net=host -it -v=/home:/home imikushin/os-installer -d /dev/sdc -t generic -c /home/core/agent-init.sh
