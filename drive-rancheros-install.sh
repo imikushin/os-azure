011bc4bfd5655277e5b01543c159eadc25eb6a36
Ivan Mikushin <i.mikushin@gmail.com>
2015-04-10 01:59:55 +0500

    use waagent-init.yml, waagent.yml

--- drive-rancheros-install.sh
+++ drive-rancheros-install.sh
@@ -4,6 +4,11 @@
 # ls -lAh /dev/disk/by-id/
 
 docker run --privileged --net=host -it --entrypoint=/scripts/set-disk-partitions imikushin/os-installer /dev/sdc
-# vi agent-init.sh
-# chmod +x agent-init.sh
-docker run --privileged --net=host -it -v=/home:/home imikushin/os-installer -d /dev/sdc -t generic -c /home/core/agent-init.sh
+
+## vi agent-init.sh
+## chmod +x agent-init.sh
+##docker run --privileged --net=host -it -v=/home:/home imikushin/os-installer -d /dev/sdc -t generic -c /home/core/agent-init.sh
+
+# vi waagent-init.yml
+# vi waagent.yml
+docker run --privileged --net=host -it -v=/home:/home imikushin/os-installer -d /dev/sdc -t generic -c /home/core/waagent-init.yml -f waagent.yml
