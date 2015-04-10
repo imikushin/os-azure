3d182ffd092cbb86a5c120b12aa30fbc4ba6bb99
Ivan Mikushin <i.mikushin@gmail.com>
2015-04-10 18:53:57 +0500

    make sshwatcher more aggressive

--- sshwatcher/watch-ssh.sh
+++ sshwatcher/watch-ssh.sh
@@ -5,8 +5,9 @@ mkdir -p /home/rancher/.ssh
 chown -R rancher:rancher /home/rancher/.ssh
 chmod -R go-rwx /home/rancher/.ssh
 
-while inotifywait -q -e attrib /home/rancher/.ssh; do
-  if [[ "$(stat -c '%U' /home/rancher/.ssh)" != "rancher" ]]; then
+while inotifywait -q -r -e attrib /home/rancher/.ssh || true; do
+  if  [[ "$(stat -c '%U' /home/rancher/.ssh)" != "rancher" ]] || \
+      [[ "$(stat -c '%U' /home/rancher/.ssh/authorized_keys)" != "rancher" ]]; then
     chown -R rancher:rancher /home/rancher/.ssh
     chmod -R go-rwx /home/rancher/.ssh
   fi
