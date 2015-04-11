691f446988f43d1f77deb98f3e18dbc269c4176f
Ivan Mikushin <i.mikushin@gmail.com>
2015-04-12 02:45:37 +0500

    do not set instance user (rancher) password

--- create-instance.sh
+++ create-instance.sh
@@ -11,6 +11,5 @@ cd $(dirname $0)
 . ./common.sh
 
 RANCHEROS_HOST=rancheros-$(apg -a 1 -n 1 -m 7 -x 7 -M NL)
-PASS=$(apg -a 1 -n 1 -m 8 -x 8 -M NLSC)
 
-azure vm create -l "West US" -e -t ./vagrantCert.pem ${RANCHEROS_HOST} ${RANCHEROS_IMAGE} rancher "${PASS}"
+azure vm create -l "West US" -e -P -t ./vagrantCert.pem ${RANCHEROS_HOST} ${RANCHEROS_IMAGE} rancher
