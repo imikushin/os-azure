f2adea196cbe747be19d0060f8aaede11a8bb8c6
Ivan Mikushin <i.mikushin@gmail.com>
2015-04-12 00:57:45 +0500

    force /home/rancher to be owned by rancher

--- sshwatcher/watch-ssh.sh
+++ sshwatcher/watch-ssh.sh
@@ -8,7 +8,7 @@ chmod -R go-rwx /home/rancher/.ssh
 while inotifywait -q -r -e attrib /home/rancher/.ssh || true; do
   if  [[ "$(stat -c '%U' /home/rancher/.ssh)" != "rancher" ]] || \
       [[ "$(stat -c '%U' /home/rancher/.ssh/authorized_keys)" != "rancher" ]]; then
-    chown -R rancher:rancher /home/rancher/.ssh
+    chown -R rancher:rancher /home/rancher
     chmod -R go-rwx /home/rancher/.ssh
   fi
 done
