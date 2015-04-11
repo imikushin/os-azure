5d730d24b56a49e93064fb84a599f721f8c4d4a6
Ivan Mikushin <i.mikushin@gmail.com>
2015-04-11 22:08:56 +0500

    script RancherOS image build

--- drive-rancheros-install.sh
+++ drive-rancheros-install.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
+set -e -x
 
-## make sure /dev/sdc exists
-# ls -lAh /dev/disk/by-id/
+cd $(dirname $0)
 
-docker run --privileged --net=host -it --entrypoint=/scripts/set-disk-partitions imikushin/os-installer /dev/sdc
+## Pre-requisites:
+## - installed packages: apg, jq
+## - you are logged in with azure cli
 
-## vi agent-init.sh
-## chmod +x agent-init.sh
-##docker run --privileged --net=host -it -v=/home:/home imikushin/os-installer -d /dev/sdc -t generic -c /home/core/agent-init.sh
+RANCHEROS_VERSION="v0.3.0-rc2-debug2"
 
-# vi waagent-init.yml
-# vi waagent.yml
-docker run --privileged --net=host -it -v=/home:/home imikushin/os-installer -d /dev/sdc -t generic -c /home/core/waagent-init.yml -f /home/core/waagent.yml
+BUILD_HOST=build-$(apg -a 1 -n 1 -m 7 -x 7 -M NL)
+BUILD_HOST_IMAGE="2b171e93f07c4903bcad35bda10acf22__CoreOS-Alpha-647.0.0"
+BUILD_HOST_USER="core"
+
+BUILD_USER_CERT=${BUILD_USER_CERT:-./vagrantCert.pem}
+BUILD_USER_KEY=${BUILD_USER_KEY:-./vagrant}
+
+azure vm create -l "West US" -e -P -t ${BUILD_USER_CERT} ${BUILD_HOST} ${BUILD_HOST_IMAGE} ${BUILD_HOST_USER}
+azure vm disk attach-new ${BUILD_HOST} 40
+
+sftp -i ${BUILD_USER_KEY} ${BUILD_HOST_USER}@${BUILD_HOST}.cloudapp.net:/home/${BUILD_HOST_USER} <<EOF
+put waagent.yml
+put rancher.yml
+EOF
+
+ssh -i ${BUILD_USER_KEY} ${BUILD_HOST_USER}@${BUILD_HOST}.cloudapp.net \
+  docker run --privileged --net=host --entrypoint=/scripts/set-disk-partitions imikushin/os-installer /dev/sdc
+
+ssh -i ${BUILD_USER_KEY} ${BUILD_HOST_USER}@${BUILD_HOST}.cloudapp.net \
+  docker run --privileged --net=host -v=/home:/home \
+    imikushin/os-installer \
+      -d /dev/sdc -t generic -c /home/${BUILD_HOST_USER}/rancher.yml -f /home/${BUILD_HOST_USER}/waagent.yml
+
+azure vm disk list --json -d ${BUILD_HOST}.cloudapp.net > ./tmp/disks.json
+
+DISK_NAME=$(cat tmp/disks.json | jq '.[1].name' | xargs -I{} echo {})
+IMAGE_VHD=$(cat tmp/disks.json | jq '.[1].mediaLink' | xargs -I{} echo {})
+
+azure vm disk detach ${BUILD_HOST} 0
+sleep 5
+
+azure vm disk delete ${DISK_NAME}
+
+azure vm image create -o linux -u ${IMAGE_VHD} RancherOS-${RANCHEROS_VERSION}
